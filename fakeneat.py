4b7a86579a3c94edf08fbaa076bdef6c52139e7e
Hugo Albert <hugoalberthlu@gmail.com>
2024-09-21 13:27:06 +0200

    v1.0 - first successful implementation for iris

--- fakeneat.py
+++ fakeneat.py
@@ -6,6 +6,7 @@ from sklearn import datasets
 from torch.nn import functional as F
 import random
 import copy
+import time
 
 def shuffle(a, b, seed):
    rand_state = np.random.RandomState(seed)
@@ -16,12 +17,13 @@ def shuffle(a, b, seed):
 @dataclass
 class Config:
 
-    proportion_train = 0.2
-    proportion_val = 0.6
+    proportion_train = 0.3
+    proportion_val = 0.5
     hidden_layers = 10
     population_size = 100
     num_generations = 10000
-    mutation_rate = 0.05
+    mutation_rate = 0.1
+    top_n = int(population_size*mutation_rate)
 
     data, target = np.float32(datasets.load_iris().data), datasets.load_iris().target
     shuffle(data, target, 42)
@@ -40,8 +42,12 @@ class Config:
               "val": (data[int(proportion_train*len(data)): int((proportion_train + proportion_val)*len(data))], target[int(proportion_train*len(target)): int((proportion_train + proportion_val)*len(target))]),
               "test": (data[int((proportion_train + proportion_val)*len(data)):], target[int((proportion_train + proportion_val)*len(target)):])}
 
+    #training parameters
+    learning_rate = 0.01
+    train_iters = 5
+
     #evaluation parameters
-    eval_iters = 100
+    eval_iters = 5
     batch_size = {"train": 32,
                   "val": 32,
                   "test": 32}
@@ -87,7 +93,7 @@ class Network(nn.Module):
                _, y_pred = x.max(dim=1)
                acc = torch.sum(y_pred == y)/len(y)
                loss = nn.CrossEntropyLoss()(x, y)
-               if Config.acc: loss = 1-acc
+               if Config.acc: loss = torch.autograd.Variable(1-acc, requires_grad= True)
             else:
                 loss = F.mse_loss(x, targets)
         return x, loss
@@ -129,8 +135,18 @@ def evaluate_fitness(indiv, split, acc = False):
     indiv.network.train() #sets to training phase, with our model it does nothing
     return out
 
-def evaluate_population(population, split = "val"):
-    return [evaluate_fitness(indiv, split) for indiv in population]
+def evaluate_population(population, suma = 0, max_fitness = 0, min_fitness = np.inf,  split = "val", top_n = False):
+    res = []
+    for indiv in population:
+        fit = evaluate_fitness(indiv, split).item()
+        res.append(fit)
+        suma += fit
+        if fit > max_fitness: max_fitness = fit
+        if fit < min_fitness: min_fitness = fit
+    if top_n:
+        indices = sorted(range(len(res)), key=lambda i: res[i], reverse=True)[:Config.top_n]
+        return res, suma, max_fitness, min_fitness, [copy.deepcopy(res[i]) for i in indices], [copy.deepcopy(population[i]) for i in indices]
+    return res, suma, max_fitness, min_fitness
 
 def mutation_add_neuron(individual):
 
@@ -158,35 +174,49 @@ def mutation_add_neuron(individual):
 
     return indiv
 
+def mutation_remove_neuron(individual): pass
+
+def mutation_mini_train(indiv):
+    individual = copy.deepcopy(indiv)
+    optimizer = torch.optim.AdamW(individual.network.parameters(), lr = Config.learning_rate)
+
+    for iter in range(Config.train_iters):
+        xb, yb = get_batch("train")
+        logits, loss = individual.network(xb, yb)
+        optimizer.zero_grad(set_to_none = True)
+        loss.backward()
+        optimizer.step()
+    return individual
+
 def crossover(parent1, parent2):
     pass
 
 def best_selection(fitness):
-    suma = np.sum(fitness)
-    max_fitness = np.max(fitness)
-    min_fitness = np.min(fitness)
-    selected = []
-    for i in range(int(Config.mutation_rate*Config.population_size)):
-        while True:
-            idx = random.randint(0, len(fitness)-1)
-            if idx not in selected and random.random() < (min_fitness + max_fitness - fitness[idx])/suma:
-                selected.append(idx)
-                break
-    return selected
+    selected = torch.multinomial(nn.Softmax(0)(torch.FloatTensor([(1 - fit) for fit in fitness])), int(Config.mutation_rate*Config.population_size))
+    return sorted(selected.tolist(), reverse= True)
 
-def worst_selection(fitness):
-    suma = np.sum(fitness)
-    selected = []
-    for i in range(int(Config.mutation_rate*Config.population_size)):
-        while True:
-            idx = random.randint(0, len(fitness)-1)
-            if idx not in selected and random.random() < fitness[idx]/suma:
-                selected.append(idx)
-                break
-    return selected
+def worst_selection(fitness, suma, min_fitness):
+    selected = torch.multinomial(nn.Softmax(0)(torch.FloatTensor([(fit - min_fitness) for fit in fitness])), 3*int(Config.mutation_rate*Config.population_size))
+    return sorted(selected.tolist(), reverse= True)
 
-def delete_worst(selected):
-    pass
+def delete_worst(selected, population, fitness, suma):
+    for idx in selected:
+        population[idx] = population[-1]
+        fitness[idx] = fitness[-1]
+        population.pop() 
+        fit = fitness.pop()
+        suma -= fit
+
+    return suma
+
+def compare_best(new_fit, best_fit, new_pop, best_pop):
+    lista = zip(new_fit + best_fit, range(len(new_fit + best_fit)))
+    ordered_list = sorted(lista)
+    fit, indices = zip(*ordered_list)
+    pop = new_pop+best_pop
+    return [copy.deepcopy(f) for f in fit[:len(best_fit)]], [copy.deepcopy(pop[i]) for i in indices[:len(best_fit)]]
+
+
 
 
 
@@ -196,17 +226,46 @@ def delete_worst(selected):
 
 if __name__ == "__main__":
     population = create_population()
-    fitness = evaluate_population(population)
+    fitness, suma, max_fitness, min_fitness, best_fitness, best_population = evaluate_population(population, top_n = True)
+    for i in range(Config.num_generations): 
+        t1 = time.time()
+        best_selected = best_selection(fitness)
+        t2 = time.time()
+        new_individuals = [mutation_add_neuron(population[i]) for i in best_selected]
+        t3 = time.time()
+        best_selected = best_selection(fitness)
+        t4 = time.time()
+        new_individuals = new_individuals + [mutation_mini_train(population[i]) for i in best_selected]
+        t5 = time.time()
+        new_fitness, suma, max_fitness, min_fitness = evaluate_population(new_individuals, suma, max_fitness, min_fitness)
+        t6 = time.time()
+        best_fitness, best_population = compare_best(new_fitness, best_fitness, new_individuals, best_population )
+        t7 = time.time()
 
-    best_selected = best_selection(fitness)
-    new_individuals = [mutation_add_neuron(population[i]) for i in best_selected]
-    new_fitness = evaluate_population(new_individuals)
+        population = population + new_individuals
+        fitness = fitness + new_fitness
 
-    population = population + new_individuals
-    fitness = fitness + new_fitness
+        t8 = time.time()
+        worst_selected = worst_selection(fitness, suma, min_fitness)
+        t9 = time.time()
+        suma = delete_worst(worst_selected, population, fitness, suma)
+        t10 = time.time()
+        population = population + best_population
+        fitness = fitness + best_fitness
+        max_fitness = np.max(fitness)
+        min_fitness = np.min(fitness)
 
-    worst_selected = worst_selection(fitness)
-    delete_worst(worst_selected)
+        print(f"End of generation {i+1}, best fitness: {min_fitness:2f}, worst fitness: {max_fitness:2f}, {len(population)}")
+        print(f"""Time Analysis:
+              - Time spent on First Selection: {t2-t1}
+              - Time spent on Adding Neurons: {t3-t2}
+              - Time spent on Mini Train: {t5-t4}
+              - Time spent on evaluating new population: {t6-t5}
+              - Time spent comparing new-best: {t7-t6}
+              - Time spent on Worst Selection: {t9-t8}
+              - Time spent on deletion: {t10-t9}""")
+
+
 
 
 
